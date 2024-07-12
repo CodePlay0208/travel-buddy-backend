8baca9d6c10ba562a8fac90e054754ae6bb847da
akshat170902 <akshat.ug20@nsut.ac.in>
2024-07-12 13:21:15 +0530

    updated trip api

--- models/TripData.js
+++ models/TripData.js
@@ -2,19 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const tripDataSchema = new mongoose.Schema({
-  key: { type: String, required: true },
   destination: { type: String, required: true },
   startDate: { type: Date, required: true },
   endDate: { type: Date, required: true },
-  details: { type: String, required: true },
   startLocation: { type: String, required: true },
   endLocation: { type: String, required: true },
   totalMembers: { type: Number, required: true },
   age: { type: Number, required: true },
   sex: { type: String, required: true },
   description: { type: String, required: true },
-  profileImg: { type: String, required: true },
-  destinationImages: [{ type: String, required: true }],
+  destinationImages: [{ type: String}],
   user: { type: Schema.Types.ObjectId, ref: 'UserProfile', required: true }
 });
 
