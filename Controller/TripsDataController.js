2dd664de418336f6e2d091d3fef7feaf04178e5c
CodePlay0208 <tusharmoudgil22@gmail.com>
2024-09-06 21:41:34 +0530

    updated create, edit, delete trip api responses

--- Controller/TripsDataController.js
+++ Controller/TripsDataController.js
@@ -53,7 +53,7 @@ const createTripHandler = asyncHandler(async (req, res) => {
     logger.info(`Trip created successfully for user ${userId}`);
     res
       .status(201)
-      .json({ trip: newtripInDatabase, allFilesUploaded: allObjectsUploaded });
+      .json({allFilesUploaded: allObjectsUploaded});
   } catch (error) {
     logger.error(`Error while creating trip: ${error.message}`);
     res.status(500).json();
@@ -237,7 +237,7 @@ const editTripHandler = asyncHandler(async (req, res) => {
 
     const updatedTrip = await tripInDatabase.save();
     logger.info(`Trip with ID: ${tripId} updated successfully`);
-    res.status(200).json({ trip: updatedTrip, allFilesUploaded });
+    res.status(200).json({  allFilesUploaded });
   } catch (err) {
     logger.error(`Error editing trip: ${err.message}`);
     res.status(500).json();
@@ -262,7 +262,7 @@ const deleteTripHandler = asyncHandler(async (req, res) => {
     await deleteObjectsFromS3Bucket(tripInDatabase.destinationImages);
     await tripInDatabase.deleteOne();
     logger.info(`Trip with ID: ${tripId} deleted successfully`);
-    res.status(200).json({ tripId });
+    res.status(200).json();
   } catch (error) {
     logger.error(`Error deleting trip: ${error.message}`);
     res.status(500).json();
