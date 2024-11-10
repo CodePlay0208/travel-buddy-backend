9e405d66fc6bbdac5f12285d05f5fca6df30df52
CodePlay0208 <tusharmoudgil22@gmail.com>
2024-11-10 17:59:57 +0530

    fix for cors

--- app.js
+++ app.js
@@ -20,12 +20,11 @@ const {
 } = require("./middleware/RequestContextMiddleware");
 
 const corsOptions = {
-  origin: process.env.ORIGIN_FOR_CLIENT,
+  origin: process.env.ORIGIN_FOR_CLIENT.split(","),
   credentials: true,
 };
 
 try {
-  logger.info("Got env var", process.env.ORIGIN_FOR_CLIENT);
   app.use(cors(corsOptions));
   app.use(express.json());
   app.use(requestContextMiddleware);
