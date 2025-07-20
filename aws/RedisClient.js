b26353d70735f748f43b6d9c9d3399c71d45a363
CodePlay0208 <tusharmoudgil22@gmail.com>
2025-07-20 15:12:13 +0530

    changed redis config

--- aws/RedisClient.js
+++ aws/RedisClient.js
@@ -1,10 +1,16 @@
-const { createClient } = require('redis');
+const { createClient } = require("redis");
 
 const redisOptions = {
-  url: process.env.REDIS_URL
+  url: process.env.REDIS_URL,
+  socket: {
+    tls: true  
+  }
 };
 
 const redisClient = createClient(redisOptions);
-redisClient.on('error', (err) => console.error('There is a error', err));
+redisClient.on("error", (err) => console.error("There is a error", err));
+redisClient.on("connect", () => {
+  console.log("✅ Redis client connected successfully");
+});
 
 module.exports = redisClient;
